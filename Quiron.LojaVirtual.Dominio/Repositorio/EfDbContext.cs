a14de8bdd9d27329834ac2f173d11399bba041fc
thydorf <thydorf@gmail.com>
2016-05-08 03:37:26 -0400

    Second commit

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidade;
+
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {      
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            ProdutosRepositorio _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+            
+            return View(produtos);
+        }
+    }
+}
