0c391fb0a0c948200e18d91f18f028ee0ea7bf88
thydorf <thydorf@gmail.com>
2016-05-08 05:59:38 -0400

    Third Commit

---
+++ Quiron.LojaVirtual.UnitTest/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Take()
+        {
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5) select num;
+            int[] teste = { 5, 4, 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+
+        [TestMethod]
+        public void Skip()
+        {
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+            int[] teste = { 1, 3, 9 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+using System.Text;
+
+namespace Quiron.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+                if(i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
